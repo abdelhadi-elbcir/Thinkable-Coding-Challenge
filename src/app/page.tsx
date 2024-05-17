e6240c08627a1338f9cc1b5e22fc046a9176c2da
abdelhadi-22 <abdoelbcir33@gmail.com>
2024-05-17 23:46:47 +0100

    final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import TopicsList from "@/components/TopicsList";
+import TopicsList from '../components/TopicsList'
 
 export default function Home() {
-  return <TopicsList />;
+  return <TopicsList/>
 }
 
